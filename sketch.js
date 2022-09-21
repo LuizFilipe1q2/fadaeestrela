d4ff5cfeb36ed32ef8fba3e5f81a4e4151389a7e
LuizFilipe1q2 <114111639+LuizFilipe1q2@users.noreply.github.com>
2022-09-21 17:59:23 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,27 +10,30 @@ const Body = Matter.Body;
 function preload()
 {
     starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
+    bgImg = loadImage("images/starNight.png");
     //carregar animação de fada 
+   fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+   fairyVoice = loadSound("sound/JoyMusic.mp3");
 }
 
 function setup() {
     createCanvas(800, 750);
 
     //escrever código para tocar o som vozFada
-
+     fairyVoice.play();
+     fairy = createSprite(130,520);
     //criar sprite de fada e adicionar animação para fada
-
-    star = createSprite(650,30);
-	star.addImage(starImg);
-	star.scale = 0.2;
+     fairy.addAnimation('fairy',fairyImg);
+     star = createSprite(650,30);
+     star.addImage(starImg);
+     star.scale = 0.2;
 
     engine = Engine.create();
-	world = engine.world;
+    world = engine.world;
 
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, starBody);
+    starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+    World.add(world, starBody);
 	
-	Engine.run(engine);
+    Engine.run(engine);
 
 }
